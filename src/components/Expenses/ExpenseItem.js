d8b082470ca2095370036b0cc0ea6ac5302e44da
Musharraf188 <mr.musharraf188@gmail.com>
2023-07-16 07:45:19 +0530

    ExpenseDate/End

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,9 @@
 import React from "react";
 import '../Expenses/ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
 const ExpenseItem = (props) => {
     return <div className="expense-item">
-        <div>{props.date}</div>
+        <ExpenseDate date={props.date}/>
         <div className="expense-item__description"><h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
         </div>
