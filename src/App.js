ae562d12243499398ee9d658606956735cdeb7c9
Musharraf188 <mr.musharraf188@gmail.com>
2023-07-16 17:22:32 +0530

    App

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import NewExpense from "./components/NewExpense/NewExpense";
 
    const EXPENSES = [
       { id:'e1', title: 'Car Insurance', date: new Date(2023, 8, 23), amount: 232.56 },
-      { id:'e2', title: 'Monthly Recharge', date: new Date(2023, 8, 23), amount: 54.56 },
-      { id:'e3', title: 'Personal Expenses', date: new Date(2023, 8, 23), amount: 897.99 },
-      { id:'e4', title: 'Coding Expenses', date: new Date(2023, 8, 23), amount: 123.84 },
-      { id:'e5', title: 'Home Expenses', date: new Date(2023, 8, 23), amount: 1435.34 }
+      { id:'e2', title: 'Monthly Recharge', date: new Date(2022, 1, 23), amount: 54.56 },
+      { id:'e3', title: 'Personal Expenses', date: new Date(2022, 7, 23), amount: 897.99 },
+      { id:'e4', title: 'Coding Expenses', date: new Date(2021, 3, 23), amount: 123.84 },
+      { id:'e5', title: 'Home Expenses', date: new Date(2020, 10  , 23), amount: 1435.34 }
    ]
    const App = () => {
    const [expenses, setExpenses] = useState(EXPENSES)
